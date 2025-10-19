cd1cbe0a192810e9de825fb9dddf240d7a10a153
Ramoware <ramdevsybscit@gmail.com>
2025-10-19 17:30:27 +0530

    fix: useSearchParams suspense error

--- next.config.js
+++ next.config.js
@@ -9,6 +9,10 @@ const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
+  // Add this to handle dynamic pages better
+  experimental: {
+    esmExternals: 'loose'
+  }
 }
 
 module.exports = nextConfig
